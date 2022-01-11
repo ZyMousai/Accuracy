4a61115fa624ed69185377700af148ef5064f459
qzc_site <616042898@qq.com>
2022-01-11 10:22:37 +0800

    修改global

--- API/app/DocumentManagement/Documents/view.py
+++ API/app/DocumentManagement/Documents/view.py
@@ -1,4 +1,7 @@
 # Documents 视图
+import shutil
+from tempfile import NamedTemporaryFile
+
 from fastapi import APIRouter, Depends, Query, File, UploadFile
 from fastapi_pagination import Page, add_pagination
 from typing import Union, Optional, List
@@ -7,7 +10,7 @@ from datetime import datetime
 from sql_models.DocumentManagement.OrmDocumentManagement import DocumentManagement
 from app.DocumentManagement.schemas import DocumentManagementModel
 from sqlalchemy.ext.asyncio import AsyncSession
-from config import GlobalsConfig
+from config import globals_config
 import os
 
 documents_router = APIRouter(
@@ -23,11 +26,18 @@ async def query_parm(filename: Optional[str] = None, start_time: Optional[str] =
 
 @documents_router.get('/', response_model=Page[DocumentManagementModel])
 async def get_docs_page(query: dict = Depends(query_parm), dbs: AsyncSession = Depends(db_session)):
+    """
+    默认获取文档页当前第一页数据
+    """
+    query['is_delete'] = 0
     return await DocumentManagement.search(query, dbs)
 
 
 @documents_router.get('/{fileid}')
 async def get_doc_one(fileid: int, dbs: AsyncSession = Depends(db_session)):
+    """
+    根据file的id来获取对应数据
+    """
     return await DocumentManagement.get_one_detail(dbs, fileid)
 
 
@@ -38,27 +48,29 @@ async def get_doc_one(fileid: int, dbs: AsyncSession = Depends(db_session)):
 #     result: DocumentManagement = (await dbs.execute(_orm)).scalars().first()
 #     return result
 
+
 @documents_router.delete('/')
 async def delete_doc(ids: Optional[List[int]] = Query(...),
                      is_logicdel: int = Query(ge=0, le=1, default=0),
                      dbs: AsyncSession = Depends(db_session)):
+    """
+    逻辑删除和真实删除
+    """
     if is_logicdel:
         return await DocumentManagement.delete_data_logic(dbs, ids)
     else:
         for doc_id in ids:
             doc_info = await DocumentManagement.get_one_detail(dbs, doc_id)
             filename = doc_info.filename
-            os.remove(os.path.join(GlobalsConfig.DocumentStoragePath, filename))
+            os.remove(os.path.join(globals_config.DocumentStoragePath, filename))
         return await DocumentManagement.delete_data(dbs, ids)
 
 
 @documents_router.post('/upload')
 async def upload_doc(user_id: int, files: List[UploadFile] = File(...), dbs: AsyncSession = Depends(db_session)):
-    '''
-    :param files:
-    :param dbs:
-    :return:
-    '''
+    """
+    文件上传
+    """
     for idx, f in enumerate(files):
         filename = f.filename
         content = await f.read()
@@ -67,7 +79,7 @@ async def upload_doc(user_id: int, files: List[UploadFile] = File(...), dbs: Asy
         doc_model = DocumentManagement(filename=filename, filesize=filesize, user_id=user_id, created_time=created_time)
         dbs.add(doc_model)
         await dbs.flush()
-        with open(os.path.join(GlobalsConfig.DocumentStoragePath, f.filename), "wb") as w:
+        with open(os.path.join(globals_config.DocumentStoragePath, f.filename), "wb") as w:
             w.write(content)
     await dbs.commit()
 
