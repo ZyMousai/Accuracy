7fa314f1acd8cc36f14dd00e8a4317ddb4f6d98e
qzc_site <616042898@qq.com>
2022-01-21 14:10:59 +0800

    文档的上传，修改，查询。

--- API/app/DocumentManagement/Documents/view.py
+++ API/app/DocumentManagement/Documents/view.py
@@ -1,10 +1,10 @@
 # Documents 视图
-from fastapi import APIRouter, Depends, Query, File, UploadFile
+from fastapi import APIRouter, Depends, Query, File, UploadFile, Path, HTTPException
 from typing import Optional, List
 from sql_models.db_config import db_session
 from datetime import datetime
 from sql_models.DocumentManagement.OrmDocumentManagement import DocumentManagement
-from app.DocumentManagement.schemas import SearchDocumentManagement
+from app.DocumentManagement.schemas import *
 from sqlalchemy.ext.asyncio import AsyncSession
 from config import globals_config
 import os
@@ -26,6 +26,7 @@ async def get_docs_page(query: SearchDocumentManagement = Depends(SearchDocument
         ("created_time", f'>"{query.start_time}"', query.start_time),
         ("created_time", f'<"{query.end_time}"', query.end_time),
         ("is_delete", '==0', 0),
+        ("department_id", f'=={query.department_id}', query.department_id),
     ]
 
     result, count, total_page = await DocumentManagement.get_all_detail_page(dbs, query.page, query.page_size,
@@ -39,7 +40,7 @@ async def get_docs_page(query: SearchDocumentManagement = Depends(SearchDocument
 
 
 @documents_router.get('/{fileid}')
-async def get_doc_one(file_id: int, dbs: AsyncSession = Depends(db_session)):
+async def get_doc_one(file_id: int = Path(..., description='文件id', ge=1), dbs: AsyncSession = Depends(db_session)):
     """
     根据file的id来获取对应数据
     """
@@ -67,34 +68,54 @@ async def delete_doc(ids: Optional[List[int]] = Query(...),
         for doc_id in ids:
             doc_info = await DocumentManagement.get_one_detail(dbs, doc_id)
             filename = doc_info.filename
-            os.remove(os.path.join(globals_config.DocumentStoragePath, filename))
+            try:
+                os.remove(os.path.join(globals_config.DocumentStoragePath, filename))
+            except:
+                print('可能是文件未同步导致的删除失败')
         return await DocumentManagement.delete_data(dbs, ids)
 
 
 @documents_router.post('/upload')
-async def upload_doc(user_id: int, files: List[UploadFile] = File(...), dbs: AsyncSession = Depends(db_session)):
+async def upload_doc(user_id: int, department_id: str, files: List[UploadFile] = File(...),
+                     dbs: AsyncSession = Depends(db_session)):
     """
     文件上传
     """
     for idx, f in enumerate(files):
         filename = f.filename
+        filter_condition = [
+            ('filename', f'=="{filename}"', filename)
+        ]
+        result = await DocumentManagement.get_one(dbs, *filter_condition)
+        if result:
+            raise HTTPException(status_code=403, detail="Duplicate filename.")
         content = await f.read()
         file_size = str(float('%.2f' % (len(content) / 1024 / 1024))) + "M"
         created_time = datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
         doc_model = DocumentManagement(filename=filename, file_size=file_size, user_id=user_id,
+                                       department_id=department_id,
                                        created_time=created_time)
         dbs.add(doc_model)
         await dbs.flush()
         with open(os.path.join(globals_config.DocumentStoragePath, f.filename), "wb") as w:
             w.write(content)
     await dbs.commit()
+    return {'file_name': [file.filename for file in files]}
 
 
-@documents_router.post('/')
-async def update_doc(ids: Optional[List[int]] = Query(...),
-                     is_logicdel: int = Query(ge=0, le=1, default=0),
+@documents_router.patch('/')
+async def update_doc(info: UpdateDocumentManagement = Depends(UpdateDocumentManagement),
                      dbs: AsyncSession = Depends(db_session)):
-    if is_logicdel:
-        return await DocumentManagement.delete_data_logic(dbs, ids)
-    else:
-        return await DocumentManagement.delete_data(dbs, ids)
+    """
+    修改文档部门id
+    :param info:
+    :param dbs:
+    :return:
+    """
+    update_data_dict = info.dict(exclude_unset=True)
+    if len(update_data_dict) > 1:
+        result = await DocumentManagement.update_data(dbs, update_data_dict, is_delete=0)
+        if not result:
+            raise HTTPException(status_code=403, detail="card does not exist.")
+    response_json = {"data": update_data_dict}
+    return response_json
