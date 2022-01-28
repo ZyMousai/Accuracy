03d0ca9a2079580926c8cdd37967de41faf30940
diy01 <dd85730312>
2022-01-28 15:15:23 +0800

    用户管理界面临时提交

--- UI/src/services/api.js
+++ UI/src/services/api.js
@@ -1,6 +1,6 @@
 //跨域代理前缀
-// const API_PROXY_PREFIX = 'http://192.168.50.115:8000/api'
-const API_PROXY_PREFIX = 'http://127.0.0.1:8000/api'
+const API_PROXY_PREFIX = 'http://192.168.50.115:8000/api'
+// const API_PROXY_PREFIX = 'http://127.0.0.1:8000/api'
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
 // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
