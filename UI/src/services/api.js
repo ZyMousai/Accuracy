eca0af99542a18250bc18baf3342ec63d9ce2b65
qzc_site <616042898@qq.com>
2022-03-02 00:15:52 -0800

    1,api上传

--- UI/src/services/api.js
+++ UI/src/services/api.js
@@ -1,5 +1,5 @@
 //跨域代理前缀
-const API_PROXY_PREFIX = 'http://192.168.50.115:8000/api'
+const API_PROXY_PREFIX = 'http://192.168.50.49:8000/api'
     // const API_PROXY_PREFIX = 'http://127.0.0.1:8000/api'
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
     // const BASE_URL = process.env.VUE_APP_API_BASE_URL
@@ -31,4 +31,5 @@ module.exports = {
     RoleMenu: `${BASE_URL}/PersonnelManagement/roles/v1/RoleMenu`,
     RolePermission: `${BASE_URL}/PersonnelManagement/roles/v1/RolePermission`,
     RoleUser: `${BASE_URL}/PersonnelManagement/roles/v1/RoleUser`,
+    CommissionConsume: `${BASE_URL}/Clerk/card/v1/statistics/`,
 }
