90976427963dee0e71641b010c68423738b532a4
qzc_site <616042898@qq.com>
2022-03-08 02:05:06 -0800

    1,账号视图的注释优化，去掉后端加密。

--- API/app/AccountManagement/Account/view.py
+++ API/app/AccountManagement/Account/view.py
@@ -19,6 +19,21 @@ account_router = APIRouter(
 @account_router.get("/")
 async def get_account(info: SearchAccount = Depends(SearchAccount),
                       dbs: AsyncSession = Depends(db_session)):
+    """
+        获取账号列表，根据传递的参数来决定列表内容
+    param info:
+
+        查询的条件，作为依赖项目，根据依赖来决定是否必传
+
+    param dbs:
+
+        数据库依赖
+
+    return:
+
+        账号列表
+
+    """
     filter_condition = [
         ('account', f'.like(f"%{info.account}%")', info.account),
         ('platform', f'.like(f"%{info.platform}%")', info.platform),
@@ -36,6 +51,21 @@ async def get_account(info: SearchAccount = Depends(SearchAccount),
 @account_router.get("/{account_id}")
 async def get_account_one(account_id: int,
                           dbs: AsyncSession = Depends(db_session)):
+    """
+        获取单条账号，传递的account_id参数为条件
+    param account_id:
+
+        在Account表里对应的是id字段，RoleAccountMapping对应的是account_id字段
+
+    param dbs:
+
+        数据库依赖
+
+    return:
+
+        单条账号的信息，额外增加了账户对应的role_id
+
+    """
     result = await Account.get_one_detail(dbs, account_id)
     filter_condition = [
         ("account_id", f"=={result.id}", result.id)
@@ -64,7 +94,22 @@ async def get_account_one(account_id: int,
 @account_router.post("/")
 async def add_account(info: AddAccount,
                       dbs: AsyncSession = Depends(db_session)):
-    info.password = encrypt(info.password)
+    """
+        新增账号
+    param info:
+
+        需要新增的字段，都必传
+
+    param dbs:
+
+        数据库依赖
+
+    return:
+
+        新增的每个字段以及字段值
+
+    """
+    # info.password = encrypt(info.password)
     account_id = await Account.add_data(dbs, info)
     response_json = info.dict()
     response_json["id"] = account_id
@@ -74,13 +119,22 @@ async def add_account(info: AddAccount,
 @account_router.patch("/")
 async def update_account(info: UpdateAccount, dbs: AsyncSession = Depends(db_session)):
     """
-    修改账号信息
-    :param info:
-    :param dbs:
-    :return:
+        修改账户
+    param info:
+
+        需要修改的字段，不是所有参数都必传
+
+    param dbs:
+
+        数据库依赖
+
+    return:
+
+        修改的每个字段以及字段值
+
     """
-    if info.password:
-        info.password = encrypt(info.password)
+    # if info.password:
+    #     info.password = encrypt(info.password)
     update_data_dict = info.dict(exclude_unset=True)
     if len(update_data_dict) > 1:
         result = await Account.update_data(dbs, update_data_dict, is_delete=0)
@@ -92,6 +146,21 @@ async def update_account(info: UpdateAccount, dbs: AsyncSession = Depends(db_ses
 
 @account_router.delete("/")
 async def delete_account(ids: Optional[List[int]] = Query(...), dbs: AsyncSession = Depends(db_session)):
+    """
+        真实删除和逻辑删除
+    param ids:
+
+        需要删除的id，可以是列表，可批量可单个
+
+    param dbs:
+
+        数据库依赖
+
+    return:
+
+        删除的数据的id
+
+    """
     result = await Account.delete_data(dbs, tuple(ids), auto_commit=False)
 
     if not result:
@@ -103,7 +172,6 @@ async def delete_account(ids: Optional[List[int]] = Query(...), dbs: AsyncSessio
     # 删除跟角色关联
     response_json = {"data": ids}
     return response_json
-
 
 # 暂时屏蔽解密接口
 # @account_router.get("/password/{account_id}")
