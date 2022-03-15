cc26f604daaef5f14477f39f6ef98a5278da53c4
diy01 <dd85730312>
2022-03-15 15:08:47 +0800

    统计卡页面上传功能临时提交

--- UI/src/pages/assistantfunction/statisticscardinformation/index.vue
+++ UI/src/pages/assistantfunction/statisticscardinformation/index.vue
@@ -41,9 +41,10 @@
                 <a-upload
                         name="file"
                         :multiple="true"
-                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        :action="updocuurl"
                         :headers="headers"
                         :showUploadList="false"
+                        :before-upload="beforeUpload"
                         @change="handleChange"
                 >
                     <a-button type="primary">
@@ -326,7 +327,7 @@
       PatchCardListData,
       PatchTaskListData,
       table_delete,
-      innerdelete, CardAccountListData, CommissionConsumetion, AddCardAccount, AddCreditTask, AddCardsExcel
+      innerdelete, CardAccountListData, CommissionConsumetion, AddCardAccount, AddCreditTask
     } from "../../../services/statisticscardinformation";
 
     const columns = [
@@ -398,6 +399,7 @@
           user: '',
         },
         uuidSelect: {},
+        updocuurl: "",
         advanced: true,
         componentKey: 0,
         selectedRows: [],
@@ -678,17 +680,21 @@
         }
         if (info.file.status === 'done') {
           console.log("done")
-          AddCardsExcel(info.file.name).then(res => {
-            // console.log("成功")
-            console.log(res)
-            this.gettabledata()
-
-          })
+          // AddCardsExcel(info.file.name).then(res => {
+          //   // console.log("成功")
+          //   console.log(res)
+          //   this.gettabledata()
+          //
+          // })
           this.$message.success(`${info.file.name} file uploaded successfully`);
         } else if (info.file.status === 'error') {
           console.log("error")
           this.$message.error(`${info.file.name} file upload failed.`);
         }
+      },
+      beforeUpload() {
+        // this.alone = process.env.VUE_APP_API_ALONE_URL
+        this.updocuurl = process.env.VUE_APP_API_BASE_URL2 + `/Clerk/card/v1/cards/excel`
       },
       selecthandleChange(data){
         console.log(data)
