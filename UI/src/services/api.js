aa52763e73c74716b7b286353baec43dcb7d1412
diy01 <dd85730312>
2022-04-12 17:42:10 +0800

    心跳api功能修改

--- UI/src/services/api.js
+++ UI/src/services/api.js
@@ -1,8 +1,8 @@
 //跨域代理前缀
 // const API_PROXY_PREFIX = 'http://45.76.15.187:8000/api'
-//     const API_PROXY_PREFIX = 'http://127.0.0.1:8000/api'
-    const API_PROXY_PREFIX = 'http://192.168.50.18:8000/api'
-    // const API_PROXY_PREFIX = 'http://192.168.50.23:8000/api'
+//     const API_PROXY_PREFIX = 'http://192.168.50.18:8000/api'
+//     const API_PROXY_PREFIX = 'http://192.168.50.18:8000/api'
+    const API_PROXY_PREFIX = 'http://127.0.0.1:8000/api'
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
     // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
