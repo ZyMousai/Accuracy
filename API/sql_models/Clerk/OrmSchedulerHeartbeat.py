7ac2172ed73cc16da7bb0a811691591dc5bc7758
diy01 <dd85730312>
2022-05-13 10:48:00 +0800

    机器表新增重启授权字段，心跳表新增类型字段

--- API/sql_models/Clerk/OrmSchedulerHeartbeat.py
+++ API/sql_models/Clerk/OrmSchedulerHeartbeat.py
@@ -15,13 +15,18 @@ class Heartbeat(PBaseModel):
     alarm = BaseType.BaseColumn(BaseType.BaseInteger, nullable=False, comment='是否启用:0-否；1-是')
     state = BaseType.BaseColumn(BaseType.BaseInteger, nullable=False, default=2, comment='状态:0-正常；1-异常；2-离线')
     at = BaseType.BaseColumn(BaseType.BaseString(510), comment='@的用户:all-所有人；手机号,手机号-被@人的手机号，英文逗号分割')
+    type = BaseType.BaseColumn(BaseType.BaseString(255), nullable=False, default="serve",
+                               comment='类型:machine-机器；serve-服务')
 
 
+# 当Heartbeat表type字段为machine时，使用此表查询机器对应ip
 class Machine(PBaseModel):
     __tablename__ = 'machine'
 
-    machine_name = BaseType.BaseColumn(BaseType.BaseString(255), nullable=False, unique=True, comment='机器ip')
+    machine_ip = BaseType.BaseColumn(BaseType.BaseString(255), nullable=False, unique=True, comment='机器ip')
     scope = BaseType.BaseColumn(BaseType.BaseString(255), nullable=False, unique=True, comment='范围,例：1-60')
+    restart_authorization = BaseType.BaseColumn(BaseType.BaseInteger, nullable=False, default=1,
+                                                comment='重启授权:0-关闭；1-开启')
 
 
 if __name__ == '__main__':
